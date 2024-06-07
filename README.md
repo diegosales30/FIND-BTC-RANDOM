51b3a1b61e2c59e7bda4ffcc810d02f4e6bf40bb
Diego sales <diegofelipesales23@gmail.com>
2024-06-07 14:46:49 -0400

    modificando para buscar chaves random

---
+++ README.md
@@ -0,0 +1,13 @@
+### How to use
+
+```
+  abra o terminal dentro do projeto
+  digite: npm install (necessário ter o node.js 20.10.0 ou superior instalado)
+  execute o arquivo: node main.js
+```
+
+```
+ Se vc quiser enviar uns satoshis caso encontre rsrs.
+ meu endereço BTC:  bc1qwt94r4sn53ze44n9crfczt0tf6wkj3na36p2hr
+
+```
---
+++ main.js
@@ -0,0 +1,111 @@
+const bitcoin = require('bitcoinjs-lib');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const crypto = require('crypto');
+const readline = require('readline');
+
+console.clear();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const btcArt = `
+\x1b[38;2;250;128;114m
+╔════════════════════════════════════════════════════════╗
+║\x1b[0m\x1b[36m   ____ _____ ____   _____ ___ _   _ ____  _____ ____   \x1b[0m\x1b[38;2;250;128;114m║
+║\x1b[0m\x1b[36m  | __ )_   _/ ___| |  ___|_ _| \\ | |  _ \\| ____|  _ \\  \x1b[0m\x1b[38;2;250;128;114m║
+║\x1b[0m\x1b[36m  |  _ \\ | || |     | |_   | ||  \\| | | | |  _| | |_) | \x1b[0m\x1b[38;2;250;128;114m║
+║\x1b[0m\x1b[36m  | |_) || || |___  |  _|  | || |\\  | |_| | |___|  _ <  \x1b[0m\x1b[38;2;250;128;114m║
+║\x1b[0m\x1b[36m  |____/ |_| \\____| |_|   |___|_| \\_|____/|_____|_| \\_\\ \x1b[0m\x1b[38;2;250;128;114m║
+║\x1b[0m\x1b[36m                                                        \x1b[0m\x1b[38;2;250;128;114m║
+╚═══\x1b[32mInvestidor Internacional & DiegoDev - RandomSearch\x1b[0m\x1b[38;2;250;128;114m═══╝\x1b[0m`;
+
+console.log(btcArt);
+
+let stopProcess = false;
+
+rl.question('Cole o endereço da Carteira: ', (chavePublicaInput) => {
+    chavePublica = chavePublicaInput; 
+    rl.question('Cole o minKey: ', (minKey) => {
+        rl.question('Cole o maxKey: ', (maxKey) => {
+            rl.question('DIGITE: (Y): ', (answer) => {
+                if (answer.toLowerCase() === 'y') {
+                    console.log('Iniciando a busca...');
+                    const minKeyBigInt = BigInt(`0x${minKey}`);
+                    const maxKeyBigInt = BigInt(`0x${maxKey}`);
+                    if (minKeyBigInt >= maxKeyBigInt) {
+                        console.log('Erro: minKey deve ser menor que maxKey.');
+                        rl.close();
+                    } else {
+                        findPrivateKey(chavePublica, minKeyBigInt, maxKeyBigInt);
+                        
+                    }
+                } else {
+                    console.log('Operação cancelada.');
+                    rl.close();
+                }
+            });
+        });
+    });
+});
+
+let chavePublica = "";
+
+// process.on('SIGINT', () => {
+//     console.log('Interrupção recebida. Encerrando...');
+//     stopProcess = true;
+//     rl.close();
+//     process.exit();
+// });
+
+function getRandomPrivateKey(min, max) {
+    const range = max - min;
+    const randomOffset = BigInt('0x' + crypto.randomBytes(16).toString('hex')) % range;
+    return min + randomOffset;
+}
+
+function checkAddress(privateKeyHex) {
+    try {
+        const key = ec.keyFromPrivate(privateKeyHex);
+        const publicKey = key.getPublic(true, 'hex'); // compressed public key
+        const { address } = bitcoin.payments.p2pkh({ pubkey: Buffer.from(publicKey, 'hex') });
+        return address === chavePublica;
+    } catch (error) {
+        console.error(`Erro ao verificar chave privada: ${error.message}`);
+        return false;
+    }
+}
+
+function findPrivateKey(chavePublica, minKey, maxKey) {
+    let attempts = 0;
+
+    while (true) {
+        const privateKey = getRandomPrivateKey(minKey, maxKey);
+        const privateKeyHex = privateKey.toString(16).padStart(64, '0');
+        attempts++;
+
+        console.log(`Tentativa #${attempts}: Chave gerada: ${privateKeyHex}`);
+
+        if (checkAddress(privateKeyHex)) {
+            console.log(`
+                ╔════════════════════════════════════════════════════════════════════════╗
+                ║ CHAVE PRIVADA ENCONTRADA:                                              ║
+                ║ ${privateKeyHex}       ║
+                ╚════════════════════════════════════════════════════════════════════════╝
+                `);
+            console.log(`Tentativas randômicas: ${attempts}`);
+            rl.close();
+            process.exit();
+            return privateKeyHex;
+        }
+
+        if (attempts % 100000 === 0) {
+            console.log(`Tentativas: ${attempts}`);
+        }
+    }
+    rl.close();
+}
+
+
